8ca84f32407e9ab3814000222e63fe083d9c64d6
Hetvi48 <hetvikhadela48@gmail.com>
2026-08-30 15:03:54 +0530

    feat: return extracted doc
    remove return content and metadata

--- ingestion/extraction/extract.py
+++ ingestion/extraction/extract.py
@@ -5,10 +5,12 @@ def load_pdf():
     pdf_path = "ingestion/data/report.pdf"
     return pdf_path
 
+# check is the file exist or not
 # if os.path.exists(pdf_path):
 #     print("yes")
 # else:
 #     print("no")
+
 def extract_pdf():
     loader = PyPDFLoader(
         file_path=load_pdf(),
@@ -17,21 +19,29 @@ def extract_pdf():
         extraction_mode='layout'
     )
 
-    docs = []
-    docs_lazy = loader.lazy_load()
-    for doc in docs_lazy:
-        docs.append(doc)
-    content = []
-    metadata = []
-    for page in docs:
-        content.append(page.page_content)
-        metadata.append(page.metadata)
-    return content, metadata
+    docs = loader.load()
+    return docs
+
+    # it is also possible to get content and metadata separately after extracting the document.
+    # docs = []
+    # docs_lazy = loader.load()
+    # for doc in docs_lazy:
+    #     docs.append(doc)
+    # content = []
+    # metadata = []
+    # for page in docs:
+    #     content.append(page.page_content)
+    #     metadata.append(page.metadata)
+    # return content, metadata
     # print(docs[0].metadata)
 
-
 if __name__ == '__main__':
-    content, metadata = extract_pdf()
-    print(content)
-    print("====================================================")
-    print(metadata)
+    docs = extract_pdf()
+    print(type(docs))
+    print(docs)
+
+    # if you use the content and metadata, uncomment the following 
+    # content, metadata = extract_pdf()
+    # print(content)
+    # print("====================================================")
+    # print(metadata)
